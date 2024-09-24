cfbc514179879217aeb9b82ceb948c99b2168154
Rigved Desai <rigveddesai@gmail.com>
2024-09-24 22:19:13 +0530

    feat: Add CORS support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,14 @@ import { PORT } from "./config/config";
 import { connectToMongoWithRetry } from "./config/mongo";
 import authRouter from "./routes/authRoutes";
 import taskRouter from "./routes/taskRoutes";
+import cors from 'cors';
 
 connectToMongoWithRetry();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ exposedHeaders: 'Authorization' }));
 
 app.get("/", (req, res) => {
   return res.status(200).json({
