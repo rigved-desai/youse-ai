d10f4db32f6a725dd5279c27738d8abdd91bea69
Rigved Desai <rigveddesai@gmail.com>
2024-09-24 10:12:42 +0530

    chore: Fix format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import { PORT } from './config/config';
-import { connectToMongoWithRetry } from './config/mongo';
+import express from "express";
+import { PORT } from "./config/config";
+import { connectToMongoWithRetry } from "./config/mongo";
 
 connectToMongoWithRetry();
 
@@ -9,11 +9,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    return res.status(200).json({
-        hello: "world" 
-    })
+  return res.status(200).json({
+    hello: "world",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port: `, PORT);
+  console.log(`Server listening on port: `, PORT);
 });
