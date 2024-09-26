fb9ea8f6e2bc38cda5f12ed6b81a8e1823f5e8cc
Rigved Desai <rigveddesai@gmail.com>
2024-09-26 05:36:53 +0530

    feat: Add JWT validation API

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,10 +3,18 @@ import {
   createUser,
   validateUserCredentials,
 } from "../controllers/authController";
+import { validateToken } from "../middleware/authMiddleware";
+import { Response, Request } from "express";
 
 const authRouter = Router();
 
 authRouter.route("/login").post(validateUserCredentials);
 authRouter.route("/sign-up").post(createUser);
+
+authRouter
+  .route("/validate-token")
+  .get(validateToken, (_: Request, res: Response) => {
+    return res.status(200).json(null);
+  });
 
 export default authRouter;
